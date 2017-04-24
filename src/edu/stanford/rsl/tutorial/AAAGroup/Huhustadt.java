bda67b138c30b848327d3f7eeaa56789bf0e9d0f
Bastian Bier <vy19nazo@stud.informatik.uni-erlangen.de>
2017-04-24 14:10:29 +0200

    yo

---
+++ src/edu/stanford/rsl/tutorial/AAAGroup/Huhustadt.java
@@ -0,0 +1,16 @@
+package edu.stanford.rsl.tutorial.AAAGroup;
+
+public class Huhustadt {
+private static void main(String [] args, int four){
+	System.out.println("huhu");
+}
+
+public static void da(){
+	String[] asd = {"hersr"};
+	main(asd,23);
+}
+
+public static void main(String[] dafjkjsdfh){
+	da();
+}
+}
